2b2b6feab079049966eeaf9f66e2f0f0695ba07b
Kate <katia230679@mail.ru>
2022-09-12 14:11:24 +0300

    task 1 and 2 added to hw lesson6

---
+++ task6.1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write($"Сколько у вас будет чисел? ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[m];
+
+void NumbersInput(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+int Compare(int[] arrayNumbers)
+{
+    int counter = 0;
+    for (int i = 0; i < arrayNumbers.Length; i++)
+    {
+        if (arrayNumbers[i] > 0) counter += 1;
+    }
+    return counter;
+}
+
+NumbersInput(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Compare(arrayNumbers)} ");
---
+++ task6.2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+double[,] factor = new double[2, 2];
+double[] point = new double[2];
+
+void FactorInput()
+{
+    for (int i = 0; i < factor.GetLength(0); i++)
+    {
+        Console.Write($"Введите значения {i + 1}-го уравнения (y = k * x + b):\n");
+        for (int j = 0; j < factor.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"Введите значение k: ");
+            else Console.Write($"Введите значение b: ");
+            factor[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+
+double[] Resolve(double[,] factor)
+{
+    point[0] = (factor[1, 1] - factor[0, 1]) / (factor[0, 0] - factor[1, 0]);
+    point[1] = point[0] * factor[0, 0] + factor[0, 1];
+    return point;
+}
+
+void OutputResolve(double[,] factor)
+{
+    if (factor[0, 0] == factor[1, 0] && factor[0, 1] == factor[1, 1])
+    {
+        Console.Write($"Прямые совпадают");
+    }
+    else if (factor[0, 0] == factor[1, 0] && factor[0, 1] != factor[1, 1])
+    {
+        Console.Write($"Прямые параллельны");
+    }
+    else
+    {
+        Resolve(factor);
+        Console.Write($"Точка пересечения: ({point[0]}, {point[1]})");
+    }
+}
+
+FactorInput();
+OutputResolve(factor);
